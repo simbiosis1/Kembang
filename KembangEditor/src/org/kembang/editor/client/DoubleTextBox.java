68afc04cbd6162ff0192072daecb059a6ebff245
Iwan Fatahi <iwanaf@gmail.com>
2015-05-13 18:09:08 +0700

    Tambah editor baru doubletextbox

---
+++ KembangEditor/src/org/kembang/editor/client/DoubleTextBox.java
@@ -0,0 +1,88 @@
+package org.kembang.editor.client;
+
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.i18n.client.NumberFormat;
+import com.google.gwt.user.client.ui.DoubleBox;
+import com.google.gwt.user.client.ui.IntegerBox;
+
+public class DoubleTextBox extends DoubleBox {
+
+	NumberFormat nf;
+	Boolean dontProcess = false;
+
+	public DoubleTextBox() {
+		nf = NumberFormat.getFormat("#,##0.00;(#,##0.00)");
+		//
+		addKeyPressHandler(new KeyPressHandler() {
+
+			@Override
+			public void onKeyPress(KeyPressEvent event) {
+				if (!Character.isDigit(event.getCharCode())
+						&& event.getNativeEvent().getKeyCode() != KeyCodes.KEY_TAB
+						&& event.getNativeEvent().getKeyCode() != KeyCodes.KEY_BACKSPACE
+						&& event.getNativeEvent().getKeyCode() != KeyCodes.KEY_LEFT
+						&& event.getNativeEvent().getKeyCode() != KeyCodes.KEY_RIGHT
+						&& event.getNativeEvent().getKeyCode() != KeyCodes.KEY_DELETE
+						&& event.getNativeEvent().getKeyCode() != KeyCodes.KEY_BACKSPACE) {
+					((IntegerBox) event.getSource()).cancelKey();
+				} else {
+					if (event.getNativeEvent().getKeyCode() == KeyCodes.KEY_LEFT
+							|| event.getNativeEvent().getKeyCode() == KeyCodes.KEY_RIGHT) {
+						dontProcess = true;
+					}
+				}
+			}
+		});
+
+		//
+		addKeyUpHandler(new KeyUpHandler() {
+
+			@Override
+			public void onKeyUp(KeyUpEvent event) {
+				if (!dontProcess) {
+					int index = getCursorPos();
+					int indexAdded = formatValue(index);
+					setCursorPos(index + indexAdded);
+				} else {
+					dontProcess = false;
+				}
+			}
+		});
+
+	}
+
+	private int formatValue(int curPos) {
+		int result = 0;
+		String text = getText();
+		int textLength = text.length();
+		int dotPos = textLength;
+		//
+		String dotText[] = text.split("\\.");
+		if (dotText.length > 2) {
+			for (int i = 0; i < dotText.length; i++) {
+				if (i == 0) {
+					text = dotText[i] + ".";
+				} else {
+					text += dotText[i];
+				}
+			}
+		}
+		if (dotText.length > 1) {
+			dotPos = dotText[0].length();
+		}
+		//
+		text = getText().replace(",", "");
+		//
+		String textFormat = nf.format(nf.parse(text));
+		int newLength = textFormat.length();
+		if (textLength != 1 && dotPos >= curPos) {
+			result = newLength - textLength;
+		}
+		setText(textFormat);
+		return result;
+	}
+}
